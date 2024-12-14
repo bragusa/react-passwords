018306c4dba85146d429df6936a6e0537f287b62
Brian Ragusa <b.v.ragusa@gmail.com>
2024-12-14 12:48:24 -0500

    finx index scss

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.scss';
 import OuterApp from './OuterApp';
 
 const root = ReactDOM.createRoot(
